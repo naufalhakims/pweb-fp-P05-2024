e21aaba2f2b55852ac3838943b68a2342fe8c651
NathanKhoP <5027231002@student.its.ac.id>
2024-12-18 16:46:10 +0700

    style: auth var name

--- backend/src/models/auth.model.ts
+++ backend/src/models/auth.model.ts
@@ -1,13 +1,13 @@
 import mongoose, { Schema, type Document } from "mongoose";
 
 export interface Auth_ extends Document {
-  username: string;
+  name: string;
   email: string;
   password: string;
 }
 
 const AuthSchema: Schema = new Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
