ada9356929ff927685f5ee0fe9f86d79633be4bf
KenasGaleno <150374973+KenasGaleno@users.noreply.github.com>
2024-12-21 21:27:39 +0700

    Update Crowdfund.vue

--- frontend/src/views/Crowdfund.vue
+++ frontend/src/views/Crowdfund.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Ini user</h1>
-</template>
+    <div>
+      <h1>All Crowdfunds</h1>
+      <div v-for="crowdfund in crowdfunds" :key="crowdfund.id" class="card">
+        <h3>{{ crowdfund.title }}</h3>
+        <p>Target: {{ crowdfund.target }}</p>
+        <p>Progress: {{ crowdfund.progress }}</p>
+        <button @click="viewDetails(crowdfund.id)">View Details</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        crowdfunds: [],
+      };
+    },
+    async created() {
+      const response = await axios.get('/api/crowdfund');
+      this.crowdfunds = response.data;
+    },
+    methods: {
+      viewDetails(id) {
+        this.$router.push(`/crowdfund/${id}`);
+      },
+    },
+  };
+  </script>
+  
